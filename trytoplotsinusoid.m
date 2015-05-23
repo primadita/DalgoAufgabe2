7ba02c9ea235a346d29a7cf5a7e776a9d9f9eefb
HelenKopter <h3l1k0pt3r61@gmail.com>
2015-05-23 18:05:19 +0200

    verbessert

--- trytoplotsinusoid.m
+++ trytoplotsinusoid.m
@@ -1,37 +1,33 @@
 function trytoplotsinusoid
 
 
-frequency = 250;
-
+time = 1;
+dt=0.01;
+frequency = 1;
 % initiate timer for animation
-t = timer('Period',1,'TimerFcn', @plotSinusoid, 'ExecutionMode', 'FixedRate');
-hfig = figure();
-start(t);uiwait(hfig);stop(t);delete(t);
+t = timer('Period',dt,'TimerFcn', @plotSinusoid, 'ExecutionMode','FixedRate', 'TasksToExecute',Inf);
+hfig=figure;
+start(t);
+uiwait(hfig);
+stop(t);delete(t);
 
 function plotSinusoid( handle, event )
 %UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-time=(1:1000);
-
+  %Detailed explanation goes here
+time=time+dt
 [X,Y] = meshgrid(-20:.25: 20);
 c=343;
-a=1;
+amplitude=1;
+T=1/frequency;
 lambda = c/frequency;
-
 r = sqrt(X.^2 + Y.^2);
-Z = a*sin(2*pi*r/lambda);
-surf(X,Y,Z); %'LineStyle','none', 'EdgeColor','none'
+Z = amplitude*sin(2*pi*(time./T+r/lambda));
+surf(X,Y,Z); %'LineStyle,'none', 'EdgeColor','none'
+
 shading(gca, 'interp');
 view(2)
+%plot(Z);
 colormap('Gray');
+drawnow;
 end
-
-
-
-
-%f=figure;
-%set(f, 'Menubar', 'none')
-
-% Auslesen der Quellpositionen
 end
