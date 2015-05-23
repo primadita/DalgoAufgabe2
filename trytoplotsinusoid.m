056865d5f77a9f07c9cab534ef16786fe8e90a6e
JanTjorbenArning <jan_tjorben_arning@t-online.de>
2015-05-23 18:21:05 +0200

    pli

--- trytoplotsinusoid.m
+++ trytoplotsinusoid.m
@@ -3,7 +3,7 @@ function trytoplotsinusoid
 
 time = 1;
 dt=0.01;
-frequency = 1;
+frequency = 100;
 % initiate timer for animation
 t = timer('Period',dt,'TimerFcn', @plotSinusoid, 'ExecutionMode','FixedRate', 'TasksToExecute',Inf);
 hfig=figure;
@@ -14,14 +14,14 @@ stop(t);delete(t);
 function plotSinusoid( handle, event )
 %UNTITLED Summary of this function goes here
   %Detailed explanation goes here
-time=time+dt
+time=time+dt;
 [X,Y] = meshgrid(-20:.25: 20);
 c=343;
 amplitude=1;
 T=1/frequency;
 lambda = c/frequency;
 r = sqrt(X.^2 + Y.^2);
-Z = amplitude*sin(2*pi*(time./T+r/lambda));
+Z = amplitude*sin(2*pi*(time./T-r/lambda));
 surf(X,Y,Z); %'LineStyle,'none', 'EdgeColor','none'
 
 shading(gca, 'interp');
