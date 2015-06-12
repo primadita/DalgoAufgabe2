e5f1beb803683dddc647112fa34902a77e4080bb
HelenKopter <h3l1k0pt3r61@gmail.com>
2015-06-13 00:54:58 +0200

    just comments

--- startbutton.m
+++ startbutton.m
@@ -1,10 +1,14 @@
+% variable to differ between running or stopped simulation
 running =1;
+
+% loading frequency, amplitude and phase from edit windows
 for cc = 1:num_source
     freq(cc) = str2double(get(hfreq(cc), 'String'));
     a0(cc)= str2double(get(hAmplitude(cc), 'String'));
     phi0(cc)= str2double(get(hPhi(cc), 'String'));
 end
 
+% initiating and starting timer for animation
 t = timer('Period',dt,'TimerFcn', 'plotSinusoid',...
           'ExecutionMode','fixedRate', 'TasksToExecute',Inf);
 start(t);
--- stopbutton.m
+++ stopbutton.m
@@ -1,1 +1,5 @@
-running=0;stop(timerfindall);delete(timerfindall);
+% variable to differ between running or stopped simulation
+running=0;
+
+% stopping and deleting all timers (simulation and lineplot)
+stop(timerfindall);delete(timerfindall);
