ab32425fc45ad4df28812d33e180521e4bff4acf
JanTjorbenArning <jan_tjorben_arning@t-online.de>
2015-06-16 10:43:37 +0200

    komentiert

--- interpoltest.m
+++ interpoltest.m
@@ -1,38 +1,21 @@
+% expects the coordinaten in the Meshgrid 
 
+% Author: J.Arning, H.Heinermann, F.Primadita
+% Ver. 0.01 initial create (empty) 23-May-2015 			 JA, HH, FP 
 function [positionMesh] =interpoltest(coordinates)
     coordinates = [coordinates(1) coordinates(2)];
+      
+    roundingVariable = 0.25; 
     
-  
-    roundingVariable = 0.25;
+    rounded = round(coordinates./roundingVariable) .* roundingVariable; %runded the coordinaten on 0,25
+    positionMesh = rounded./roundingVariable; %position in the Meshgrid is calculated
 
-    
-    rounded = round(coordinates./roundingVariable) .* roundingVariable;
-    positionMesh = rounded./roundingVariable;
-
-        switch 'positionMesh'
+        switch 'positionMesh' %position is seting on the right place
             case 0
-                positionMesh = [81, 81];
+                positionMesh = [81, 81]; 
             otherwise
                 positionMesh = positionMesh + [81, 81];            
         end
     
-% %--------------------------------------------------------------------------
-%     rounded = round(coordinatesx/roundingVariable) * roundingVariable;
-%     positionMeshx = rounded/roundingVariable;
-% 
-%         switch positionMeshx
-%             case 0
-%                 positionMeshx = 81;
-%             otherwise
-%                 positionMeshx = positionMeshx + 81;            
-%         end
-%  
-%         roundedy = round(coordinatesy/roundingVariable) * roundingVariable;
-%         positionMeshy = roundedy/roundingVariable;
-%         switch positionMeshy
-%             case 0
-%                 positionMeshy = 81; 
-%             otherwise
-%                 positionMeshy = positionMeshy + 81;     
-%         end
+
 end
